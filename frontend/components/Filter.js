db9cec633029af38a73e0259d30857e1b5263f11
Martin Zelník <Martin.Zelnik@ibm.com>
2021-06-02 09:00:29 +0200

    filter connected to app context

--- frontend/components/Filter.js
+++ frontend/components/Filter.js
@@ -1,13 +1,15 @@
 import styles from '../styles/Filter.module.scss';
 import { Button, ContentSwitcher, Select, SelectItem, Switch, TextInput } from 'carbon-components-react';
 import { List16, Grid16 } from '@carbon/icons-react';
+import { TABS, useAppContext, VIEWS } from '../context/AppProvider';
 
 export default function Filter({ pokemonTypes }) {
+  const { setView, setActiveTab } = useAppContext();
   return (
     <div className={styles.filter}>
-      <ContentSwitcher onChange={console.log}>
-        <Switch name={'all'} text="All" />
-        <Switch name={'favourites'} text="Favourites" />
+      <ContentSwitcher onChange={value => setActiveTab(value.index)}>
+        <Switch name={TABS.ALL} text="All" />
+        <Switch name={TABS.FAVORITES} text="Favourites" />
       </ContentSwitcher>
       <TextInput placeholder="Search"></TextInput>
       <Select noLabel>
@@ -17,8 +19,22 @@ export default function Filter({ pokemonTypes }) {
         ]}
       </Select>
       <div>
-        <Button kind="secondary" size="field" renderIcon={List16} hasIconOnly iconDescription="List" />
-        <Button kind="secondary" size="field" renderIcon={Grid16} hasIconOnly iconDescription="Grid" />
+        <Button
+          onClick={() => setView(VIEWS.LIST)}
+          kind="secondary"
+          size="field"
+          renderIcon={List16}
+          hasIconOnly
+          iconDescription="List"
+        />
+        <Button
+          onClick={() => setView(VIEWS.GRID)}
+          kind="secondary"
+          size="field"
+          renderIcon={Grid16}
+          hasIconOnly
+          iconDescription="Grid"
+        />
       </div>
     </div>
   );
