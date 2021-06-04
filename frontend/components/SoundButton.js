7446a032039da77764fc769d1f5cf9d93a11f080
Martin Zelník <Martin.Zelnik@ibm.com>
2021-06-04 15:53:13 +0200

    custom hook use sound that tracks is the sound is being played implemented

--- frontend/components/SoundButton.js
+++ frontend/components/SoundButton.js
@@ -1,12 +1,35 @@
 import styles from '../styles/TransparentButton.module.scss';
-import { VolumeUp32 } from '@carbon/icons-react';
+import { VolumeUp32, VolumeUpFilled32 } from '@carbon/icons-react';
+import { useEffect, useState } from 'react';
+
+const useAudio = sound => {
+  const [audio] = useState(new Audio(sound));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
 
 export default function SoundButton({ sound }) {
-  const audio = new Audio(sound)
+  const [playing, toggle] = useAudio(sound);
 
   return (
-    <button onClick={() => audio.play()} className={styles['transparent-button']}>
-      <VolumeUp32 />
+    <button onClick={toggle} className={styles['transparent-button']}>
+      {playing ? <VolumeUpFilled32 /> : <VolumeUp32 />}
     </button>
   );
 }
